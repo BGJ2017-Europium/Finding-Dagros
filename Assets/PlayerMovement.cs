4745f272ad7407d7be139089d6633459511a9265
ArcticFqx <dan-eric.naess@live.no>
2017-01-21 03:01:24 +0100

    Initial Multiplayer

---
+++ Assets/PlayerMovement.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class PlayerMovement : NetworkBehaviour
+{
+
+
+    void Update()
+    {
+        if (!isLocalPlayer)
+            return;
+
+        var x = Input.GetAxis("Horizontal");
+        var z = Input.GetAxis("Vertical");
+        
+
+
+        transform.Translate(x, 0, z);
+    }
+}
